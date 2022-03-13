36d6b97663a18e23a58327cf9a6045086c042372
dahae0320 <ldh990320ldh@gmail.com>
2022-03-14 00:58:44 +0900

    Create Bookky Project & add gitignore

---
+++ BookkyProject-iOS/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookkyProject-iOS
+//
+//  Created by Da Hae Lee on 2022/03/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookkyProject-iOSUITests/BookkyProject_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BookkyProject_iOSUITestsLaunchTests.swift
+//  BookkyProject-iOSUITests
+//
+//  Created by Da Hae Lee on 2022/03/13.
+//
+
+import XCTest
+
+class BookkyProject_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
