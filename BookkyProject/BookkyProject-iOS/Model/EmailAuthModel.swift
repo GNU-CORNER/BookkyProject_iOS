f47187da27ea12111b0b64cf1cc6a687cf95be3c
dahae0320 <ldh990320ldh@gmail.com>
2022-04-05 16:52:39 +0900

    ✅ #13 : Add EmailAuthModel File

---
+++ BookkyProject/BookkyProject-iOS/Model/EmailAuthModel.swift
@@ -0,0 +1,20 @@
+//
+//  EmailAuthModel.swift
+//  BookkyProject-iOS
+//
+//  Created by Da Hae Lee on 2022/04/05.
+//
+
+import Foundation
+
+// MARK: - EmailAuthModel
+struct EmailAuthModel: Codable {
+    let success: Bool
+    let result: Result
+    let errorMessage: String
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let email: String
+}
