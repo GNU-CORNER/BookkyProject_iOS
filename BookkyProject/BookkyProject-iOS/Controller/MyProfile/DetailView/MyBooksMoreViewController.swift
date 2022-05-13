8d677e0a643c64f137a13d08d99eea5810938c2d
dahae0320 <ldh990320ldh@gmail.com>
2022-05-13 16:48:05 +0900

    ✅ #21 : Implement MyBooks view. ➕ #21 : Add Image asset. Update my profile model.

--- BookkyProject/BookkyProject-iOS/Controller/MyProfile/DetailView/MyBooksMoreViewController.swift
+++ BookkyProject/BookkyProject-iOS/Controller/MyProfile/DetailView/MyBooksMoreViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class MyBooksMoreViewController: UIViewController {
 
+    @IBOutlet weak var myBooksMoreCollectionView: UICollectionView!
+    struct myBooksMoreModel {
+        var image: UIImage
+        var title: String
+    }
+    var myBooksMoreArray: [myBooksMoreModel] = [
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1"),
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1"),
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1"),
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1"),
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1"),
+        myBooksMoreModel(image: UIImage(named: "생각하는북키1")!, title: "생각하는북키1")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.myBooksMoreCollectionView.delegate = self
+        self.myBooksMoreCollectionView.dataSource = self
+        setDefaultView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setDefaultView() {
+//        self.navigationController?.navigationBar.tintColor = UIColor(named: "Label Color")
+        self.navigationController?.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareButtonTapped))
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationItem.title = "관심도서 보기"
     }
-    */
+    
+    @objc func shareButtonTapped() {
+        // nothing else ^^
+    }
+}
 
+extension MyBooksMoreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myBooksMoreArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyBooksMoreCell", for: indexPath) as? MyBooksMoreCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        cell.bookThumbnailImageView.image = myBooksMoreArray[indexPath.row].image
+        cell.booksTitleLabel.text = myBooksMoreArray[indexPath.row].title
+        return cell
+    }
+    
+    
 }
