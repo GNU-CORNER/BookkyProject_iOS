b5d66087ba9deb38a884229dbf8a492ee65da5ac
dahae0320 <ldh990320ldh@gmail.com>
2022-05-13 23:16:51 +0900

    ➕ #21 : Implement UpdateProfile View layout item.

---
+++ BookkyProject/BookkyProject-iOS/Controller/MyProfile/DetailView/MyProfileUpdateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyProfileUpdateViewController.swift
+//  BookkyProject-iOS
+//
+//  Created by Da Hae Lee on 2022/05/13.
+//
+
+import UIKit
+
+class MyProfileUpdateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.topItem?.title = "프로필 수정"
+    }
+    
+
+}
