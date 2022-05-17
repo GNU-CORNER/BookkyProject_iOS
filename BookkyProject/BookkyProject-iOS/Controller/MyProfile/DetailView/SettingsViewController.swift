be488fac4a5980cf46666cbc37598fae09bcc0c3
dahae0320 <ldh990320ldh@gmail.com>
2022-05-17 16:33:25 +0900

    ✅ #26 : SettingsTableView, Static Cell로 만듦

--- BookkyProject/BookkyProject-iOS/Controller/MyProfile/DetailView/SettingsViewController.swift
+++ BookkyProject/BookkyProject-iOS/Controller/MyProfile/DetailView/SettingsViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var settingsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "설정"
+        self.navigationController?.navigationBar.topItem?.title = ""
     }
     
 
