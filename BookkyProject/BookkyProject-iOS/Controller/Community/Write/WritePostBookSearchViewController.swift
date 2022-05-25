d52f5d46490d6bb250414eec206d0fbd536a34ab
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-05-25 14:43:19 +0900

    변경 테스트

--- BookkyProject/BookkyProject-iOS/Controller/Community/Write/WritePostBookSearchViewController.swift
+++ BookkyProject/BookkyProject-iOS/Controller/Community/Write/WritePostBookSearchViewController.swift
@@ -14,19 +14,39 @@ class WritePostBookSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBookSearchTableView()
+
     }
     
     @IBAction func tapCancelViewButton(_ sender: UIButton) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
+    private func setSearchBar(){
+        let searchController = UISearchController(searchResultsController: self)
+        searchController.searchBar.showsScopeBar = true
+        self.navigationItem.searchController = searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+    }
     private func setBookSearchTableView(){
         self.bookSearchTableView.delegate = self
         self.bookSearchTableView.dataSource = self
     }
+
 }
 extension WritePostBookSearchViewController : UITableViewDelegate ,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.bookSearchTableView.frame.width, height: 50))
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.showsScopeBar = true
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        headerView.addSubview(searchController.searchBar)
+        return headerView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
