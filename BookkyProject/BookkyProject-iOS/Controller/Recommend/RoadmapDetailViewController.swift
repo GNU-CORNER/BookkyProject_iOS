08636bd8a90bd49d35df6303ac6f5aa49349349a
dahae0320 <ldh990320ldh@gmail.com>
2022-08-27 21:32:15 +0900

    ✅ #39 : RoadmapDetailViewController -> topicView clear.

--- BookkyProject/BookkyProject-iOS/Controller/Recommend/RoadmapDetailViewController.swift
+++ BookkyProject/BookkyProject-iOS/Controller/Recommend/RoadmapDetailViewController.swift
@@ -9,20 +9,30 @@ import UIKit
 
 class RoadmapDetailViewController: UIViewController {
     
+    var topic = ""
+    var question = ""
+    
     @IBOutlet weak var topicView: UIView!
     @IBOutlet weak var topicLabel: UILabel!
     @IBOutlet weak var questionTitle: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     
-    var topic = ""
-    var question = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.navigationController?.navigationBar.topItem?.title = ""
         self.topicLabel?.text = topic
         self.questionTitle?.text = question
+        
+        setTopicView()
     }
-
+    
+    private func setTopicView() {
+        topicView.layer.borderWidth = 1.0
+        topicView.layer.borderColor = UIColor(named: "primaryColor")?.cgColor
+        topicView.layer.cornerRadius = topicView.frame.height / 2
+        topicLabel.sizeToFit()
+//        topicLabel.frame.width = topicLabel.frame.width + 20
+    }
+    
 }
